13df48e7398d349a5f482ea8d28d15ce1188838b
Javier <javier.fuentesm@hotmail.com>
2020-11-16 17:35:37 -0500

    Ex 1 extra credit3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,9 +5,13 @@ import React, {useReducer} from 'react'
 // function countReducer(previousCount, newCount) {
 //   return previousCount + newCount
 // }
-function countReducer(previousCount, newCount) {
-  return {...previousCount, ...newCount}
-}
+// function countReducer(previousCount, newCount) {
+//   return {...previousCount, ...newCount}
+// }
+const countReducer = (state, action) => ({
+  ...state,
+  ...(typeof action === 'function' ? action(state) : action),
+})
 function Counter({initialCount = 0, step = 1}) {
   // const [count, setCount] = useReducer(countReducer, initialCount)
   //   const increment = () => setCount(step)
@@ -15,7 +19,9 @@ function Counter({initialCount = 0, step = 1}) {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  // const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
